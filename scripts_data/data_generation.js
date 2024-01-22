4c948cb54ba6f3bee8ab67f85f88e57a742acc63
Quentin Vincenot <quentin.vincenot@gmail.com>
2024-01-22 20:29:13 +0100

    Retrieved base stats of Pikachu

--- scripts_data/data_generation.js
+++ scripts_data/data_generation.js
@@ -4,7 +4,7 @@
 
 // Function to initialise data in a global manner, that will serve for all visualisations
 function startup_initialise_data() {
-    let promise = fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur');
+    let promise = fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
     promise.then(response => {
         console.log(response);
         if(response.status == 200) {
@@ -13,7 +13,24 @@ function startup_initialise_data() {
         }
     }).then(data => {
         console.log(data);
-        console.log(data['types'])
+        console.log(data['types']);
+        console.log(data['stats']);
+
+        let pokemon_stats = {
+            "hp": 0, "attack": 0, "defense": 0, "special-attack": 0, "special-defense": 0, "speed": 0
+        }
+
+        for(let i=0; i<data['stats'].length; i++) {
+            let stat_name = data['stats'][i]['stat']['name'];
+            pokemon_stats[stat_name] = data['stats'][i]['base_stat']
+        }
+
+        document.getElementById('pikachu-hp').innerHTML = JSON.stringify(pokemon_stats['hp']);
+        document.getElementById('pikachu-atk').innerHTML = JSON.stringify(pokemon_stats['attack']);
+        document.getElementById('pikachu-def').innerHTML = JSON.stringify(pokemon_stats['defense']);
+        document.getElementById('pikachu-spa').innerHTML = JSON.stringify(pokemon_stats['special-attack']);
+        document.getElementById('pikachu-sdf').innerHTML = JSON.stringify(pokemon_stats['special-defense']);
+        document.getElementById('pikachu-spd').innerHTML = JSON.stringify(pokemon_stats['speed']);
     })
 }
 
