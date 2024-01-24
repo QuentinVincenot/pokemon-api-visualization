8704ac552467e279213125aad455d3c85ce915c1
Quentin Vincenot <quentin.vincenot@gmail.com>
2024-01-24 21:34:18 +0100

    Changed initial data retrieval process to synchronous

--- scripts_data/data_research.js
+++ scripts_data/data_research.js
@@ -3,6 +3,39 @@
 --------------------------------------------------------------------------------*/
 
 //
+function display_search_suggestions() {
+    
+    console.log(POKEMON_DATA.length);
+    console.log(POKEMON_SORTED_NAMES.length);
+
+
+    // Retrieve the text written in the search bar and convert it to lower case
+    /*let search_bar_content = document.getElementById('search_bar').value.toLowerCase();
+    console.log('Searched :', search_bar_content);
+
+    let matching_names = [], matching_names_number = 0, current_search_id = 0;
+
+    while(matching_names_number < 5) {
+        if(POKEMON_SORTED_NAMES[current_search_id].startsWith(search_bar_content)) {
+            matching_names.push(POKEMON_SORTED_NAMES[current_search_id]);
+            matching_names_number += 1;
+        }
+        current_search_id += 1;
+    }*/
+
+    /*while(matching_names_number < 5 && current_search_id < POKEMON_SORTED_NAMES.length) {
+        console.log(search_bar_content, " * ", POKEMON_SORTED_NAMES[current_search_id]);
+        if(search_bar_content != "" && POKEMON_SORTED_NAMES[current_search_id].startsWith(search_bar_content)) {
+            matching_names.push(POKEMON_SORTED_NAMES[current_search_id]);
+            matching_names_number += 1;
+        }
+        current_search_id += 1;
+    }*/
+
+    //console.log('Found :', matching_names);
+}
+
+//
 function search_for_pokemon() {
     // Retrieve the text written in the search bar and convert it to lower case
     let search_bar_content = document.getElementById('search_bar').value.toLowerCase();
@@ -30,6 +63,11 @@ function display_searched_pokemon_data(pokemon_array) {
         document.getElementById('pokemon_stats').innerHTML = JSON.stringify(pokemon_data['Stats'])
     }
 }
+
+
+//
+const search_bar = document.getElementById('search_bar');
+search_bar.addEventListener('keyup', display_search_suggestions);
 
 // Retrieve the search button and map its click behaviour on the research function
 const search_button = document.getElementById('search_button');
--- scripts_data/data_retrieval.js
+++ scripts_data/data_retrieval.js
@@ -3,50 +3,56 @@
 --------------------------------------------------------------------------------*/
 
 // Function to retrieve data about Pokémons (names, types, stats, images, etc.)
-function retrieve_pokemon_data() {
+async function retrieve_pokemon_data() {
+    // Disable the search bar while data is completely retrieved from the API
+    document.getElementById('search_bar').disabled = true;
+    document.getElementById('search_bar').style.backgroundColor = "#CCC";
+
     // First API call to retrieve the list of every Pokémon
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=1025&offset=0').then(response => {
-        if(response.status == 200) {
-            let json_response = response.json();
-            return json_response;
-        }
-    }).then(data => {
-        // Once we got the list of every Pokémon, we can iterate over them to retrieve more information
-        for(let pokemon_id=1; pokemon_id<=data['results'].length; pokemon_id++) {
-            // Retrieve the current Pokémon name
-            let pokemon_name = data['results'][pokemon_id-1]['name'];
-            // Second API call to retrieve for a single Pokémon many information, especially its base statistics
-            fetch(data['results'][pokemon_id-1]['url']).then(response => {
-                if(response.status == 200) {
-                    let json_response = response.json();
-                    return json_response;
-                }
-            }).then(pokemon_data => {
-                // Retrieve the base statistics of the current Pokémon
-                let pokemon_stats = pokemon_data['stats'];
-                // Build the complete Pokémon data object with retrieved information
-                let current_pokemon_data = {
-                    'ID': pokemon_id,
-                    'Name': pokemon_name,
-                    'Stats': {
-                        'HP':               pokemon_stats[0]['base_stat'],
-                        'Attack':           pokemon_stats[1]['base_stat'],
-                        'Defense':          pokemon_stats[2]['base_stat'],
-                        'Special Attack':   pokemon_stats[3]['base_stat'],
-                        'Special Defense':  pokemon_stats[4]['base_stat'],
-                        'Speed':            pokemon_stats[5]['base_stat'],
-                    }
-                };
-                // Save the current Pokémon retrieved information within a global list
-                POKEMON_DATA.push(current_pokemon_data);
-            });
-        }
-    }).then(() => {
-        console.log(POKEMON_DATA);
-    });
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1025&offset=0');
+    let data = await response.json();
+
+    // Once we got the list of every Pokémon, we can iterate over them to retrieve more information
+    for(let pokemon_id=1; pokemon_id<=data['results'].length; pokemon_id++) {
+        // Retrieve the current Pokémon name
+        let pokemon_name = data['results'][pokemon_id-1]['name'];
+        
+        // Second API call to retrieve for a single Pokémon many information, especially its base statistics
+        let response2 = await fetch(data['results'][pokemon_id-1]['url']);
+        let pokemon_data = await response2.json();
+
+        // Retrieve the base statistics of the current Pokémon
+        let pokemon_stats = pokemon_data['stats'];
+        // Build the complete Pokémon data object with retrieved information
+        let current_pokemon_data = {
+            'ID': pokemon_id,
+            'Name': pokemon_name,
+            'Stats': {
+                'HP':               pokemon_stats[0]['base_stat'],
+                'Attack':           pokemon_stats[1]['base_stat'],
+                'Defense':          pokemon_stats[2]['base_stat'],
+                'Special Attack':   pokemon_stats[3]['base_stat'],
+                'Special Defense':  pokemon_stats[4]['base_stat'],
+                'Speed':            pokemon_stats[5]['base_stat'],
+            }
+        };
+        // Save the current Pokémon retrieved information within a global list
+        POKEMON_DATA.push(current_pokemon_data);
+    }
+
+    // Put every Pokémon names in a single and sorted list to display suggestions on search
+    for(let i=0; i<POKEMON_DATA.length; i++) {
+        console.log(POKEMON_DATA[i]['Name'].toLowerCase());
+        POKEMON_SORTED_NAMES.push(POKEMON_DATA[i]['Name'].toLowerCase());
+    }
+    POKEMON_SORTED_NAMES.sort();
+
+    // Reactivate the search bar while the data retrieval process is finished
+    document.getElementById('search_bar').disabled = false;
+    document.getElementById('search_bar').style.backgroundColor = "#FFF";
 }
 
 
 // Call the data retrieval function to get intitial Pokémon data from online source (pokeapi.co)
-let POKEMON_DATA = [];
+let POKEMON_DATA = [], POKEMON_SORTED_NAMES = [];
 retrieve_pokemon_data();
