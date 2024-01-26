2cf5eba340cf60255e942fea03ac4372faf1deaf
Quentin Vincenot <quentin.vincenot@gmail.com>
2024-01-26 08:46:04 +0100

    Improved the search behaviour with suggestion selection and display/hide

--- scripts_data/data_research.js
+++ scripts_data/data_research.js
@@ -24,11 +24,37 @@ function display_search_suggestions() {
     
     // Display suggestions based on the current search
     for(let i=0; i<matching_names.length; i++) {
+        // Create suggestion item under the search bar
         let suggestion_item = document.createElement('li');
         let displayed_search = '<b>' + search_bar_content.charAt(0).toUpperCase() + search_bar_content.slice(1) + '</b>';
         suggestion_item.innerHTML = displayed_search + matching_names[i].substr(from=search_bar_content.length);
+        // Add a click event listener to perform the search when selecting a suggestion
+        suggestion_item.addEventListener('click', select_suggestion);
+        // Display the suggestion in a list
         suggestions_area.appendChild(suggestion_item);
     }
+}
+
+//
+function select_suggestion(event) {
+    // Retrieve the clicked suggestion with the click event
+    let clicked_suggestion = event.target;
+    // Retrieve the parent node if we got the bold part of the suggestion
+    if(event.target.tagName == "B") {
+        clicked_suggestion = event.target.parentNode;
+    }
+
+    // Escape the bold tags in the suggestion text to get the complete Pokémon name
+    let escaped_suggestion = clicked_suggestion.innerHTML.replace('<b>','').replace('</b>', '');
+    // Replace the searched value in the search bar by the complete Pokémon name
+    let search_bar_content = document.getElementById('search_bar');
+    search_bar_content.value = escaped_suggestion;
+    // Hide the previously displayed suggestions
+    let suggestions_area = document.getElementById('search_suggestions');
+    suggestions_area.innerHTML = '';
+    
+    // Perform the search with the selected suggested Pokémon
+    search_for_pokemon();
 }
 
 //
